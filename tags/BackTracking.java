5333ab9b8b3494d79c766d669b73f336b25be38f
陈欣怡 <falcon@ChensMac.local>
2017-04-23 21:45:33 -0500

    4

---
+++ tags/BackTracking.java
@@ -0,0 +1,64 @@
+/*
+526. Beautiful Arrangement
+Suppose you have N integers from 1 to N. We define a beautiful arrangement as an array that is constructed by these N numbers successfully if one of the following is true for the ith position (1 ≤ i ≤ N) in this array:
+
+The number at the ith position is divisible by i.
+i is divisible by the number at the ith position.
+Now given N, how many beautiful arrangements can you construct?
+
+Example 1:
+Input: 2
+Output: 2
+Explanation: 
+
+The first beautiful arrangement is [1, 2]:
+
+Number at the 1st position (i=1) is 1, and 1 is divisible by i (i=1).
+
+Number at the 2nd position (i=2) is 2, and 2 is divisible by i (i=2).
+
+The second beautiful arrangement is [2, 1]:
+
+Number at the 1st position (i=1) is 2, and 2 is divisible by i (i=1).
+
+Number at the 2nd position (i=2) is 1, and i (i=2) is divisible by 1.
+Note:
+N is a positive integer and will not exceed 15.
+*/
+//Try all the possible combinations and use an array to track used elements;
+public class Solution{
+	int count = 0; //use gloable variable to keep results 
+	public int countArrangement(int N) {
+		if (N < 1) return 0;
+		int[] used = new int[N + 1];
+		countHelper(N, 1, used);
+		return count;
+	}
+	public void countHelper(int N, int pos, int[] used) {
+		if (pos > N) {
+			count++; //means the whole combination array is beautiful;
+			return;
+		}
+		for (int i = 1; i <= N; i++) {
+			if (used[i] == 0 && (i % pos == 0 || pos % i == 0)){
+				used[i] = 1;
+				countHelper(N, pos + 1, used);
+				used[i] = 0;
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- tags/DynamicProgramming.java
+++ tags/DynamicProgramming.java
@@ -1,4 +1,49 @@
 /*
+413. Arithmetic Slices
+A sequence of number is called arithmetic if it consists of at least three elements and if the difference between any two consecutive elements is the same.
+
+For example, these are arithmetic sequence:
+
+1, 3, 5, 7, 9
+7, 7, 7, 7
+3, -1, -5, -9
+The following sequence is not arithmetic.
+
+1, 1, 2, 5, 7
+
+A zero-indexed array A consisting of N numbers is given. A slice of that array is any pair of integers (P, Q) such that 0 <= P < Q < N.
+
+A slice (P, Q) of array A is called arithmetic if the sequence:
+A[P], A[p + 1], ..., A[Q - 1], A[Q] is arithmetic. In particular, this means that P + 1 < Q.
+
+The function should return the number of arithmetic slices in the array A.
+
+Example:
+
+A = [1, 2, 3, 4]
+
+return: 3, for 3 arithmetic slices in A: [1, 2, 3], [2, 3, 4] and [1, 2, 3, 4] itself.
+*/
+/*[1, 2, 3, 4], sum = 3, cur = 3, add 5 to the sequecne, we created a new arithmetic array[3, 4, 5]
+also each old arithmetic array can create a new a array, because old ones + new element is a new one
+so update the sum = sum + cur; */
+public class Solution{
+	public int numberOfArithmeticSlices(int[] A) {
+		if (A == null || A.length < 3) return 0;
+		int cur = 0, sum = 0;
+		for (int i = 2; i < A.length; i++) {
+			if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+				cur += 1;
+				sum += cur; //key trick
+			}else {
+				cur = 0;
+			}
+		}
+		return sum;
+	}
+}
+
+/*
 338. Counting Bits
 Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation and return them as an array.
 
@@ -12,4 +57,16 @@ Space complexity should be O(n).
 Can you do it like a boss? Do it without using any builtin function like __builtin_popcount in c++ or in any other language.
 */
 //Dynamic Programming: f[i] = f[i / 2] + i % 2;
-
+//The last set bit always contributes one in the counter, then we can unset the bit and solve the problem recursively.
+//Right shit by 1 bit, compare to previously, the number of set bit would either reduce by 1(when number is odd) or no change(when number is even).
+//O(N),O(N)
+public class Solution{
+	public int[] countBits(int num) {
+		if (num < 0) return null;
+		int[] dp = new int[num + 1];
+		for (int i = 1; i <= num; i++) {
+			dp[i] = dp[i >>> 1] + (i & 1);
+		}
+		return dp;
+	}
+}
