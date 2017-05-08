0a028e6d8173a7156517c6710d3e1ea7d7092401
“Cindy <xinyichen2018@u.northwestern.edu>
2017-05-08 18:36:28 -0500

    array

--- tags/Array.java
+++ tags/Array.java
@@ -1,4 +1,235 @@
 /*
+238. Product of Array Except Self
+Given an array of n integers where n > 1, nums, return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+Solve it without division and in O(n).
+
+For example, given [1,2,3,4], return [24,12,8,6].
+
+Follow up:
+Could you solve it with constant space complexity? (Note: The output array does not count as extra space for the purpose of space complexity analysis.)
+*/
+/*
+//left
+Iter a, i > 0:
+	a[i] = production of left elements
+		 = a[i-1] * a[i-1] production of left elements;
+//right
+Iter a backwards, i < a.length - 1:
+	a[i] *= production of right elements
+		  = a[i + 1] * a[i+1] production of left elements;
+return a[i];
+
+Test Case:
+a:[1,2,3,4]
+res: [2*3*4, 1*3*4, 1*2*4, 1*2*3]
+
+i:1, p *= nums[0] = 1 = 1, [0,1,0,0]
+i:2, p *= 1*2 , [0,1,1*2,0]
+i:3, p *= 1*2*3, [0,1,1*2,1*2*3]
+
+i:2 p = 4, [0,1,1*2 * 4,1*2*3]
+i:1 p = 4*3, [0,1 * 4*3,1*2 * 4,1*2*3]
+i:0 p = 4*3*2, [4*3*2,1 * 4*3,1*2 * 4,1*2*3]
+
+
+*/
+public class Solution{
+	public int[] productExceptSelf(int[] nums) {
+		if (nums == null) return null;
+		int[] res = new int[nums.length];
+		res[0] = 1;
+		
+		//left
+		int p = 1;
+		for (int i = 1; i < nums.length; i++) {
+			p *= nums[i - 1];
+			res[i] = p;
+		}
+		//right
+		p = 1;
+		for (int i = nums.length - 2; i >= 0; i--) {
+			p *= nums[i + 1];
+			res[i] *= p;
+		}
+
+		return res;
+    } 
+}
+
+/*
+245. Shortest Word Distance III
+This is a follow up of Shortest Word Distance. The only difference is now word1 could be the same as word2.
+
+Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
+
+word1 and word2 may be the same and they represent two individual words in the list.
+
+For example,
+Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+
+Given word1 = “makes”, word2 = “coding”, return 1.
+Given word1 = "makes", word2 = "makes", return 3.
+
+Note:
+You may assume word1 and word2 are both in the list.
+*/
+/*
+w1 = w2 = -1;
+Iterate words:
+	if words[i] == word1 and word2:
+		min = Math.min(min, i - w1);
+		w1 = i;
+	else if words[i] == word1:
+		w1 = i;
+	else if words[i] == word2:
+		w2 = i;
+	else continue;
+
+	if (w1 and w2 != -1)
+		min = Math.min(min, w1- w2);
+
+Test Case:
+words = ["practice", "makes", "perfect", "coding", "makes"]
+Given word1 = "makes", word2 = "makes", return 3.
+i: 1, w1 = 1, w2 = -1, min = max;
+i: 4, w1 = 4, w2 = -1, min = 4 - 1 = 3;
+*/
+public class Solution{
+	public int shortestWordDistance(String[] words, String word1, String word2) {
+		int min = Integer.MAX_VALUE;
+		if (words == null) return min;
+		int w1 = -1, w2 = -1;
+		boolean same = word1.equals(word2);
+		for (int i = 0; i < words.length; i++) {
+			if (words[i].equals(word1) && same) {
+				if (w1 != -1)
+					min = Math.min(min, i - w1);
+				w1 = i;
+			}else if (words[i].equals(word1)) {
+				w1 = i;
+			}else if (words[i].equals(word2)) {
+				w2 = i;
+			}else continue;
+
+			//If w1 or w2 has changed, update min
+			if (w1 != -1 && w2 != -1)
+				min = Math.min(min, Math.abs(w1 - w2));
+		}
+		return min;
+
+	}
+}
+
+/*
+243. Shortest Word Distance
+Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
+
+For example,
+Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+
+Given word1 = “coding”, word2 = “practice”, return 3.
+Given word1 = "makes", word2 = "coding", return 1.
+
+Note:
+You may assume that word1 does not equal to word2, and word1 and word2 are both in the list.
+*/
+/*
+min = Integer.MAX_VALUE;
+f1 = f2 = -1;
+Iterate words:
+	if words[i] == w1:
+		f1 = i;
+	if words[i] == w2:
+		f2 = i;
+	if (f1 & f2 > 0)
+		min = Math.min(min, Math.abs(f1-f2));
+return min;
+
+Test Case:
+words = ["practice", "makes", "perfect", "coding", "makes"]
+w1: "makes", w2: "coding"
+
+i:1, w1 = 1, w2 = -1 < 0, min = max;
+i:3, w1 = 1, w2 = 3, min = 2;
+i:4, w1 = 4, w2 = 3, min = 1;
+return 1;
+*/
+public class Solution{
+	public int shortestDistance(String[] words, String word1, String word2) {
+		if (words == null) return 0;
+		int minDistance = Integer.MAX_VALUE;
+		int w1 = -1, w2 = -1;
+		
+		for (int i = 0; i < words.length; i++) {
+			if (words[i].equals(word1)) 
+				w1 = i;
+			else if (words[i].equals(word2)) 
+				w2 = i;
+			else continue;
+			//Once w1 or w2 changed, update the min distance
+			if (w1 >= 0 && w2 >= 0)
+				minDistance = Math.min(minDistance, Math.abs(w1 - w2));
+		}
+		return minDistance;
+	}
+}
+/*
+495. Teemo Attacking
+In LLP world, there is a hero called Teemo and his attacking can make his enemy Ashe be in poisoned condition. Now, given the Teemo's attacking ascending time series towards Ashe and the poisoning time duration per Teemo's attacking, you need to output the total time that Ashe is in poisoned condition.
+
+You may assume that Teemo attacks at the very beginning of a specific time point, and makes Ashe be in poisoned condition immediately.
+
+Example 1:
+Input: [1,4], 2
+Output: 4
+Explanation: At time point 1, Teemo starts attacking Ashe and makes Ashe be poisoned immediately. 
+This poisoned status will last 2 seconds until the end of time point 2. 
+And at time point 4, Teemo attacks Ashe again, and causes Ashe to be in poisoned status for another 2 seconds. 
+So you finally need to output 4.
+Example 2:
+Input: [1,2], 2
+Output: 3
+Explanation: At time point 1, Teemo starts attacking Ashe and makes Ashe be poisoned. 
+This poisoned status will last 2 seconds until the end of time point 2. 
+However, at the beginning of time point 2, Teemo attacks Ashe again who is already in poisoned status. 
+Since the poisoned status won't add up together, though the second poisoning attack will still work at time point 2, it will stop at the end of time point 3. 
+So you finally need to output 3.
+Note:
+You may assume the length of given time series array won't exceed 10000.
+You may assume the numbers in the Teemo's attacking time series and his poisoning time duration per attacking are non-negative integers, which won't exceed 10,000,000.
+*/
+/*
+a: [1,2] 
+t: 2
+Iterate a:
+	if a[i] - a[i - 1] > t:
+		totalTime += t;
+	else 
+		totalTime+= a[i] - a[i - 1]
+return totalTime + t;
+
+Test Case:
+a: [1,2] 
+t: 2
+
+i = 1, a[1]-a[0] = 2 - 1 = 1 < 2, total+= 1 = 1;
+return total+2 = 3;
+*/
+public class Solution{
+	public int findPoisonedDuration(int[] timeSeries, int duration) {
+		if (timeSeries == null || timeSeries.length == 0 || duration < 1) return 0;
+		int poisonedDuration = 0;
+		for (int i = 1; i < timeSeries.length; i++) {
+			int t = timeSeries[i] - timeSeries[i - 1];
+			if (t < duration) poisonedDuration += t;
+			else poisonedDuration += duration;
+		}
+		return poisonedDuration + duration;
+	}
+}
+
+/*
 448. Find All Numbers Disappread in an Array
 Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
 
