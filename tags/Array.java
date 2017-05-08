f62328e9a1eb1ac4731b1612e119a0346e2cdfee
xychen1993 <xinyichen2018@u.northwestern.edu>
2017-05-08 18:50:31 -0500

    array

--- tags/Array.java
+++ tags/Array.java
@@ -31,8 +31,6 @@ i:3, p *= 1*2*3, [0,1,1*2,1*2*3]
 i:2 p = 4, [0,1,1*2 * 4,1*2*3]
 i:1 p = 4*3, [0,1 * 4*3,1*2 * 4,1*2*3]
 i:0 p = 4*3*2, [4*3*2,1 * 4*3,1*2 * 4,1*2*3]
-
-
 */
 public class Solution{
 	public int[] productExceptSelf(int[] nums) {
