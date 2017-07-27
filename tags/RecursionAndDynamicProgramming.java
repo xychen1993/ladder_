7e960555abf5eeed137f8d0d9d161869154ede3f
xychen1993 <xinyichen2018@u.northwestern.edu>
2017-07-26 19:59:51 -0500

    recursion

--- tags/RecursionAndDynamicProgramming.java
+++ tags/RecursionAndDynamicProgramming.java
@@ -1,4 +1,119 @@
 /*
+9.4 Write a method to return all subsets of a set.
+*/
+/*
+set: [1, 2, .. n]
+base case:
+0 {}
+1 {}, {1}
+2 {}, {1}, {1,2}, {2}
+3 {}, {1}, {1,2}, {2}, {3}, {1,3}, {1,2,3}, {2,3}
+
+subsets(set):
+	if set is empty:
+		return {}
+	preSub = subsets(set - last element)
+	return preSub + each elem in preSub.append(last element)
+
+subsets(set):
+	list.add({});
+	iter e in set:
+		pre = all elements in list
+		newSubsets = add e into each pre
+		list.add(newSubsets)
+	return list;
+*/
+public class Solution{
+	public List<List<Integer>> getSubsets(List<Integer> set) {
+		/*Recursion*/
+		recursiveGetSubsets(set, set.size() - 1); //start from the first element
+		/*Iteration*/
+		iterateGetSubsets(set);
+
+	}
+	public List<List<Integer>> recursiveGetSubsets(List<Integer> set, int index) {
+		if (set == null) return null;
+		List<ArrayList<Integer>> allSubsets = new ArrayList<ArrayList<Integer>>();
+		
+		if (index == 0) {
+			allSubsets.add(new ArrayList<Integer>());
+			return allSubsets;
+		}
+
+		int curInt = set.get(index - 1);
+		List<ArrayList<Integer>> preSubsets = recursiveGetSubsets(set, index - 1);
+		List<ArrayList<Integer>> newSubsets = new ArrayList<ArrayList<Integer>>();
+		for (List<Integer> preSub : preSubsets) {
+			ArrayList<Integer> newSub = new ArrayList<>();
+			newSub.addAll(preSub);
+			newSub.add(curInt);
+			newSubsets.add(newSub);
+		}
+		newSubsets.addAll(preSubsets); 
+		
+		return newSubsets;
+
+	}
+
+}
+
+/*
+9.3 A magic index in an array A[1...n-1] is defined to be an index such that A[i] = i. Given a sorted array of distinct integers, write a method to find a magic index, if one exists, in array A.
+
+FOLLOW UP
+What if the values are not distinct?
+*/
+/*
+A = [-1,0, 1, 3, 5, 7]
+	[0, 1, 2, 3, 4, 5]
+
+getMagicIndex(A):
+	if i > A[i]:
+		search left part
+	if i < A[i]:
+		search right part
+	if i == A[i] 
+		return i;
+
+follow up:
+A = [0, 0, 4, 4, 4, 4]
+	[0, 1, 2, 3, 4, 5]
+
+getMagicIndex(A):
+	if (midIndex == midValue) return midIndex;
+	check left part from max(midIndex, midValue);
+	check right part from min(midIndex, midValue);
+*/
+public class Solution{
+	public int getMagicIndex(int[] A) {
+		if (A == null) return -1;
+		return getMagicIndex(A, 0, A.length - 1);
+	}
+
+	public int getMagicIndex(int[] A, int start, int end) {
+		if (start > end || start < 0 || end >= A.length) {
+			return -1;
+		}
+		
+		int midIndex = start + (end - start) / 2;
+		int midValue = A[midIndex];
+
+		if (midIndex == midValue) {
+			return midValue;
+		}
+
+		int left = getMagicIndex(A, start, Math.min(midValue, midIndex - 1));
+		if (left != -1) {
+			return left;
+		}
+		
+		int right = getMagicIndex(A, Math.max(midValue, midIndex + 1), end);
+
+		return right;
+	}
+}
+
+/*
 9.2 Imagine a robot sitting on the upper left corner of an X by Y grid. The robot can only move in two directions: right and down. How many possible paths are there for the robot to go from (0, 0) to (X, Y) ?
 
 FOLLOW UP
@@ -114,7 +229,7 @@ nth stair:
 3 from n-3
 the sum of the number of ways of reaching each of the last three steps.
 
-
+//time is exponet O(3^N) cuz each step branches out 3 more calls
 waysToRunUpStairs(n):
 	if n < 1 return 0;
 	if n == 1 return 1;
@@ -137,6 +252,7 @@ n result
 				3. 2 + 1
 
 */
+
 public class Solution{
 	public int countWaysRunStairs(int n) {
 		if (n <= 0) return 0;
