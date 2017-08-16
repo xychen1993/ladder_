aca457e85b8f8667358f691e4bd5ff0055cf261a
xychen1993 <xinyichen2018@u.northwestern.edu>
2017-08-16 18:49:59 -0500

    bfs

--- ladder/4_BFS.java
+++ ladder/4_BFS.java
@@ -299,11 +299,447 @@ public class Solution {
         return false;
     }
 }
+/*
+R6. Knight Shortest Path 
+Given a knight in a chessboard (a binary matrix with 0 as empty and 1 as barrier) with a source position, find the shortest path to a destination position, return the length of the route. 
+Return -1 if knight can not reached.
 
+Notice
+source and destination must be empty.
+Knight can not enter the barrier.
 
+Clarification
+If the knight is at (x, y), he can get to the following positions in one step:
+(x + 1, y + 2)
+(x + 1, y - 2)
+(x - 1, y + 2)
+(x - 1, y - 2)
+(x + 2, y + 1)
+(x + 2, y - 1)
+(x - 2, y + 1)
+(x - 2, y - 1)
 
+Example
+[[0,0,0],
+ [0,0,0],
+ [0,0,0]]
+source = [2, 0] destination = [2, 2] return 2
 
+[[0,1,0],
+ [0,0,0],
+ [0,0,0]]
+source = [2, 0] destination = [2, 2] return 6
 
+[[0,1,0],
+ [0,0,1],
+ [0,0,0]]
+source = [2, 0] destination = [2, 2] return -1
+*/
+/**
+ * Definition for a point.
+ * public class Point {
+ *     publoc int x, y;
+ *     public Point() { x = 0; y = 0; }
+ *     public Point(int a, int b) { x = a; y = b; }
+ * }
+ */
+//O(COL * ROW)
+public class Solution {
+    public boolean EMPTY = false;
+    public boolean BARRIER = true;
+    public int shortestPath(boolean[][] grid, Point source, Point destination) {
+        if (grid == null || source == null || destination == null) {
+            return -1;
+        }
+        //put source into queue
+        int steps = 0;
+        Queue<Point> q = new LinkedList<>();
+        q.offer(source);
+        grid[source.x][source.y] = BARRIER; //mark as visited
+        int[] directionX = new int[]{1, 1, -1, -1, 2, 2, -2, -2};
+        int[] directionY = new int[]{2, -2, 2, -2, 1, -1, 1, -1};
+        //check next positons until reach at destination
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                Point curr = q.poll();
+                if (curr.x == destination.x && curr.y == destination.y) {
+                    return steps;
+                }
+                for (int j = 0; j < 8; j++) {
+                    int x = curr.x + directionX[j];
+                    int y = curr.y + directionY[j];
+                    if (!inBound(x, y, grid) || grid[x][y] == BARRIER) {
+                        continue;
+                    }
+                    q.offer(new Point(x, y));
+                    grid[x][y] = BARRIER;
+                }
+            }
+            steps++;
+
+        }
+        return -1;
+    }
+    private boolean inBound(int x, int y, boolean[][] grid) {
+        if (grid.length == 0) {
+            return false;
+        }
+        return x >= 0 && x < grid.length && y >= 0 && y < grid[0].length;
+    }
+}
+/*
+R7. Course Schedule II 
+There are a total of n courses you have to take, labeled from 0 to n - 1.
+Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+
+Given the total number of courses and a list of prerequisite pairs, return the ordering of courses you should take to finish all courses.
+
+There may be multiple correct orders, you just need to return one of them. If it is impossible to finish all courses, return an empty array.
+
+Example
+Given n = 2, prerequisites = [[1,0]]
+Return [0,1]
+
+Given n = 4, prerequisites = [1,0],[2,0],[3,1],[3,2]]
+Return [0,1,2,3] or [0,2,1,3]
+*/
+/*
+注意一定要先给每个课程建立空的list，否则有的课不在prerequisites数组里出现，其实就是indegree = 0，但会导致上不了！
+*/
+//O(N + E)
+public class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        if (numCourses < 1 || prerequisites == null) {
+            return new int[0];
+        }
+        //get mapping(course -> following courses), indegree[] to keep track of indegrees of courses
+        int[] order = new int[numCourses];
+        Map<Integer, ArrayList<Integer>> map = new HashMap<>();
+        int[] indegrees = initial(numCourses, prerequisites, map);
+        //Do bfs to take courses, return num of taken courses
+        int courseTaken = takeCourse(map, indegrees, order);
+        //check if taken course = n
+        if (courseTaken == numCourses) {
+            return order;
+        }
+        return new int[0];
+    }
+    /*get mapping(course -> following courses), indegree[] to keep track of indegrees of courses*/
+    private int[] initial(int numCourses, int[][] prerequisites, Map<Integer, ArrayList<Integer>> map) {
+        int[] indegrees = new int[numCourses];
+        /*一定要先给每个课建list，否则有的课不在数组里出现，其实就是indegree = 0，但会导致上不了*/
+        for (int i = 0; i < numCourses; i++) {
+            map.put(i, new ArrayList<Integer>());
+        }
+        for (int[] pair : prerequisites) {
+            int pre = pair[1];
+            int post = pair[0];
+            indegrees[post]++;
+            ArrayList<Integer> following = map.get(pre);
+            following.add(post);
+            map.put(pre, following);
+        }
+        return indegrees;
+    }
+    /* mark course in queue as taken, and indegree of their next following course - 1, put next untaken courses which have 0 prerequisite into queue*/
+    private int takeCourse(Map<Integer, ArrayList<Integer>> map, int[] indegrees, int[] order) {
+        int courseTaken = 0;
+        Queue<Integer> q = new LinkedList<>();
+        // put courses which have 0 prerequisite into queue
+        for (int i = 0; i < indegrees.length; i++) {
+            if (indegrees[i] == 0) {
+                q.offer(i);
+                order[courseTaken++] = i;
+            }
+        }
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            for (int post : map.get(curr)) {
+                indegrees[post]--;
+                if (indegrees[post] == 0) {
+                    q.offer(post);
+                    order[courseTaken++] = post;
+                }
+            }
+        }
+        return courseTaken;
+    }
+}
+/*
+R8. Binary Tree Serialization 
+
+Design an algorithm and write code to serialize and deserialize a binary tree. Writing the tree to a file is called 'serialization' and reading back from the file to reconstruct the exact same binary tree is 'deserialization'.
+
+Notice
+There is no limit of how you deserialize or serialize a binary tree, LintCode will take your output of serialize as the input of deserialize, it won't check the result of serialize.
+
+Example
+An example of testdata: Binary tree {3,9,20,#,#,15,7}, denote the following structure:
+
+  3
+ / \
+9  20
+  /  \
+ 15   7
+Our data serialization use bfs traversal. This is just for when you got wrong answer and want to debug the input.
+
+You can use other method to do serializaiton and deserialization.
+*/
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+class Solution {
+    /*serialize binary tree into level order traversal values*/
+    public String serialize(TreeNode root) {
+        String serialized = "";
+        if (root == null) {
+            return serialized;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        serialized += root.val;
+        while (!q.isEmpty()) {
+            TreeNode curr = q.poll();
+            if (curr.left != null) {
+                q.offer(curr.left);
+                serialized += "," + curr.left.val;
+            } else {
+                serialized += "," + "#";
+            }
+            if (curr.right != null) {
+                q.offer(curr.right);
+                serialized += "," + curr.right.val;
+            } else {
+                serialized += "," + "#";
+            }
+        }
+        return serialized;
+    }
+     /*deserialize level order traversal into a binary tree*/
+     /*遍历string数组，不是#就建立新node放入list，用index来记录当前走到第几个node，每遍历两个string index + 1*/
+    public TreeNode deserialize(String data) {
+        if (data == null || data.length() == 0) {
+            return null;
+        }
+        String[] values = data.split(",");
+        ArrayList<TreeNode> nodes = new ArrayList<>();
+        int index = 0;
+        boolean isLeftChild = true;
+        nodes.add(new TreeNode(Integer.parseInt(values[0])));
+        for (int i = 1; i < values.length; i++) {
+            String value = values[i];
+            if (!value.equals("#")) {
+                TreeNode node = nodes.get(index);
+                TreeNode child = new TreeNode(Integer.parseInt(value));
+                nodes.add(child);
+                if (isLeftChild) {
+                    node.left = child;
+                } else {
+                    node.right = child;
+                }
+            }
+            isLeftChild = !isLeftChild;
+            if (isLeftChild) {
+                index++;
+            }
+        }
+        return nodes.get(0);
+    }
+}
+/*
+R9. Graph Valid Tree 
+Given n nodes labeled from 0 to n - 1 and a list of undirected edges (each edge is a pair of nodes), write a function to check whether these edges make up a valid tree.
+
+ Notice
+
+You can assume that no duplicate edges will appear in edges. Since all edges are undirected, [0, 1] is the same as [1, 0] and thus will not appear together in edges.
+
+Example
+Given n = 5 and edges = [[0, 1], [0, 2], [0, 3], [1, 4]], return true.
+
+Given n = 5 and edges = [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]], return false.
+*/
+public class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        //1. num of edges = n - 1; 2. All the nodes are connected;
+        if (edges == null) {
+            return false;
+        }
+        //get the num of edges, check 1
+        int edgesNum = edges.length;
+        if (edgesNum != n - 1) {
+            return false;
+        }
+        if (n == 1) {
+            return true;
+        }
+        //get map<node->neighbors>
+        Map<Integer, ArrayList<Integer>> map = initGraph(n, edges);
+        //check if all the nodes are connected by doing BFS, check 2
+        return isConnected(n, map);
+    }
+    private Map<Integer, ArrayList<Integer>> initGraph(int n, int[][] edges) {
+        Map<Integer, ArrayList<Integer>> map = new HashMap<>();
+        for (int[] edge : edges) {
+            int node1 = edge[0];
+            int node2 = edge[1];
+            if (!map.containsKey(node1)) {
+                map.put(node1, new ArrayList<Integer>(Arrays.asList(node2)));
+            } else {
+                ArrayList<Integer> neighbors = map.get(node1);
+                neighbors.add(node2);
+                map.put(node1, neighbors);
+            }
+            if (!map.containsKey(node2)) {
+                map.put(node2, new ArrayList<Integer>(Arrays.asList(node1)));
+            } else {
+                ArrayList<Integer> neighbors = map.get(node2);
+                neighbors.add(node1);
+                map.put(node2, neighbors);
+            }
+        }
+        return map;
+    }
+    /*check if all the nodes are connected by doing BFS*/
+    private boolean isConnected(int n, Map<Integer, ArrayList<Integer>> map) {
+        Set<Integer> visited = new HashSet<>();
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(0);
+        visited.add(0);
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            if (!map.containsKey(curr)) {
+                return false;
+            }
+            for (int neighbor : map.get(curr)) {
+                if (visited.contains(neighbor)) {
+                    continue;
+                }
+                visited.add(neighbor);
+                q.offer(neighbor);
+            }
+        }
+        return visited.size() == n;
+    }
+}
+/*
+R10. Build Post Office II 
+Given a 2D grid, each cell is either a wall 2, an house 1 or empty 0 (the number zero, one, two), find a place to build a post office so that the sum of the distance from the post office to all the houses is smallest.
+
+Return the smallest sum of distance. Return -1 if it is not possible.
+
+Notice
+You cannot pass through wall and house, but can pass through empty.
+You only build post office on an empty.
+
+Example
+Given a grid:
+
+0 1 0 0 0
+1 0 0 2 1
+0 1 0 0 0
+return 8, You can build at (1,1). (Placing a post office at (1,1), the distance that post office to all the house sum is smallest.)
+*/
+/*
+从每个empty出发去寻找，可能empty太多，所以从每个house出发去寻找答案
+     * 1. 先拿到所有的list of houses 和 empties
+     * 2. 遍历hosues，计算所有house到每个empty的距离，叠加起来，并且记录有多少hosue来visit过
+     * 3. 遍历emties，找到既被所有house visited，也是叠加距离最小的
+*/
+class Cell {
+    int x;
+    int y;
+    public Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+public class Solution {
+    public int HOUSE = 1;
+    public int EMPTY = 0;
+    public int WALL = 2;
+    public int shortestDistance(int[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return -1;
+        }
+        //get all the hosues and empties
+        List<Cell> houses = getList(HOUSE, grid);
+        List<Cell> empties = getList(EMPTY, grid);
+        //iter houses, search for the distance from current house to all empties
+        int[][] distanceSum = new int[grid.length][grid[0].length];
+        int[][] visitedTimes = new int[grid.length][grid[0].length];
+        for (Cell house : houses) {
+            getDistanceSum(house, grid, distanceSum, visitedTimes);
+        }
+        //iter houses empties, looking for the smallest sum, also all houses have visited this empty
+        int houseNum = houses.size();
+        int minDistanceSum = Integer.MAX_VALUE;
+        for (Cell empty : empties) {
+            if (visitedTimes[empty.x][empty.y] != houseNum) {
+                continue;
+            }
+            minDistanceSum = Math.min(minDistanceSum, distanceSum[empty.x][empty.y]);
+        }
+        //check if there's a solution
+        if (minDistanceSum == Integer.MAX_VALUE) {
+            return -1;
+        }
+        return minDistanceSum;
+    }
+    private List<Cell> getList(int type, int[][] grid) {
+        List<Cell> l = new ArrayList<>();
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == type) {
+                    l.add(new Cell(i, j));
+                }
+            }
+        }
+        return l;
+    }
+    private void getDistanceSum(Cell cell, int[][] grid, int[][] distanceSum, int[][] visitedTimes) {
+        Queue<Cell> q = new LinkedList<>();
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        int steps = 0;
+        int[] directionX = new int[]{0, 1, 0, -1};
+        int[] directionY = new int[]{1, 0, -1, 0};
+        q.offer(cell);
+        visited[cell.x][cell.y] = true;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            steps++;
+            for (int i = 0; i < size; i++) {
+                Cell curr = q.poll();
+                for (int j = 0; j < 4; j++) {
+                    int x = curr.x + directionX[j];
+                    int y = curr.y + directionY[j];
+                    if (!inBound(x, y, grid) || grid[x][y] != EMPTY || visited[x][y]) {
+                        continue;
+                    }
+                    q.offer(new Cell(x, y));
+                    distanceSum[x][y] += steps;
+                    visitedTimes[x][y] += 1;
+                    visited[x][y] = true;
+                }
+            }
+        }
+    }
+    private boolean inBound(int x, int y, int[][] grid) {
+        if (grid.length == 0) {
+            return false;
+        }
+        return x >= 0 && x < grid.length && y >= 0 && y < grid[0].length;
+    }
+}
 
 
 
